2ac5f62f483c129d772c563b4d93e816395f5837
sinan <sinan@XXX>
2024-12-09 00:10:35 +0300

    Add project files.

---
+++ TravelTripProje/Models/Class/Hakkimizda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Class
+{
+    public class Hakkimizda
+    {
+        public int Id { get; set; }
+        public string FotoUrl { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
